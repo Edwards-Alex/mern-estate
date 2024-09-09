3d449f4f35e718ee2067c7de6fab98a4713013d0
Edwards-Alex <1032607679w@gmail.com>
2024-09-09 10:53:59 +0800

    create pages and routes

--- fronted/src/App.jsx
+++ fronted/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import SignIn from './pages/Signin.jsx'
+import SignUp from './pages/SignUp.jsx'
+import About from './pages/About.jsx'
+import Profile from './pages/Profile.jsx'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-red-500'>Hello world !!!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
