a9daca3973015cd8c6c5133bf645ec8370d36757
Edwards-Alex <1032607679w@gmail.com>
2024-09-14 12:17:07 +0800

    create sign in API route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import userModel from "../models/User.model.js";
 import bcryptsjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,6 +11,26 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json("User created successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const vaildUser = await userModel.findOne({ email });
+    if (!vaildUser) return next(errorHandler(404, "User not found!"));
+    const vaildPassword = bcryptsjs.compareSync(password, vaildUser.password);
+    if (!vaildPassword) return next(errorHandler(401, "Wrong credential!"));
+    const token = jwt.sign({ id: vaildUser._id }, process.env.JWT_SECRET);
+    //seprate the password for safe
+    const { password: pass, ...rest} = vaildUser._doc;
+    //save this token as browser cookie
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- fronted/src/pages/SignUp.jsx
+++ fronted/src/pages/SignUp.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import {React, useState} from 'react'
 import { Link } from 'react-router-dom'
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e)=> {
+    setFormData({
+      ...formData,
+      [e.target.id]:e.target.value,
+    });
+  }
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    const res = await fetch('/api/auth/signup',{
+      method: 'POST',
+      hearders:{
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify(formData),
+    });  
+    const data = await res.json();
+    
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4 justify-center'>
-        <input type="text" placeholder='username'
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4 justify-center'>
+        <input type="text" placeholder='username' onClick={handleChange}
         className='border p-3 rounded-lg' id='username'/>
-        <input type="email" placeholder='email'
+        <input type="email" placeholder='email' onClick={handleChange}
         className='border p-3 rounded-lg' id='email'/>
-        <input type="password" 
+        <input type="password" placeholder='password'  onClick={handleChange}
         className='border p-3 rounded-lg' id='password'/>
         <button className='bg-slate-700 text-white p-3 
         rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign Up</button>
@@ -19,7 +40,7 @@ const SignUp = () => {
         <p>Have an account?</p>
         <Link to={'/sign-in'}>
           <span className='text-blue-700'>Sign in</span>
-        </Link>
+        </Link> onClick={handleChange}
       </div>
     </div>
   )
