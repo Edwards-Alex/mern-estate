9f0c539523394122666bb53b49fc4134ea0ba8d6
Edwards-Alex <1032607679w@gmail.com>
2024-11-14 09:30:05 +0800

    complete  about page

--- fronted/src/pages/About.jsx
+++ fronted/src/pages/About.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      About
+    <div className=' max-w-6xl mx-auto py-20 px-4 mb-14'>
+      <h1 className='text-3xl font-bold mb-8 text-slate-700'>About Sahand Estate</h1>
+      <p className="text-slate-800 mb-14">At Sahand Estate, we are committed to providing exceptional real estate services to our valued clients. With years of experience in the real estate market, we specialize in residential, commercial, and investment properties, offering personalized solutions that align with your needs. Our team of dedicated professionals works tirelessly to ensure that your property journey is smooth, transparent, and efficient. Whether you're looking to buy, sell, rent, or invest, we pride ourselves on delivering superior results with the utmost integrity.</p>
+      <p className="text-slate-800 mb-14" mb-14>Our extensive portfolio includes a wide range of properties in prime locations, with each listing carefully selected to meet the diverse preferences and investment goals of our clients. From luxurious homes in serene neighborhoods to thriving commercial spaces, Sahand Estate brings you opportunities that maximize value and provide long-term growth potential. Our expert team offers in-depth market insights, helping you make informed decisions while navigating the ever-changing real estate landscape. We understand that each client is unique, and we take the time to understand your specific requirements, guiding you every step of the way through the buying, selling, or leasing process.</p>
+      <p className="text-slate-800 mb-14">At Sahand Estate, customer satisfaction is our top priority. Our mission is not just to meet expectations but to exceed them. We believe in creating lasting relationships built on trust and reliability. By providing timely updates, transparent communication, and comprehensive support, we ensure that your real estate experience is as seamless as possible. Whether you're a first-time homebuyer, a seasoned investor, or a business looking for the perfect commercial property, Sahand Estate is here to make your real estate dreams a reality.</p>
     </div>
   )
 }
